150437272b5774d8af0bc6d669f4f609b39f44b8
advitt <advitt5090@yandex.com>
2024-07-26 19:01:14 +0300

    update

--- ui_test.py
+++ ui_test.py
@@ -1,8 +1,11 @@
-# Здесь прописан пока только вызов ui
 
 import sys
+import minecraft_launcher_lib
 from PySide6.QtWidgets import QApplication, QMainWindow
-from untitled import Ui_MainWindow
+from ZOVUI import Ui_MainWindow
+import subprocess
+
+minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
 
 
 class ExpenseTracker(QMainWindow):
@@ -11,8 +14,27 @@ class ExpenseTracker(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.ui.pushButton_4.clicked.connect(self.close)
+
+        self.ui.PlayButtonJAVA.clicked.connect(self.run)
+
+
         self.setStyleSheet(f"QMainWindow {{ border-image: url('Image (1).png') 0 0 0 0 stretch stretch; }}")
+
+    def run(self):
+
+        version = '1.16.5'
+        username = 'advitt'
+
+        minecraft_launcher_lib.install.install_minecraft_version(version, minecraft_directory)
+
+
+        options = {
+            'username': username
+        }
+
+
+        minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(version, minecraft_directory, options)
+
 
 
 if __name__ == "__main__":
