f478672bed8352749acd1394f258904132e84abd
Kilka <alak6543@gmail.com>
2024-07-28 02:46:04 +0300

    Добавление Kivy

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
-import sys
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.lang import Builder
+from kivy.core.window import Window
+from uuid import uuid1
 import minecraft_launcher_lib
 import subprocess
 
 
-minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
-
-def run():
-    version = input('vers')
-    username = input('name')
+Window.size = (960, 540)
 
 
-    minecraft_launcher_lib.install.install_minecraft_version(versionid=version, minecraft_directory=minecraft_directory)
-    options = {
-        'username': username,
-        'uuid': str(uuid1()),
-        'token': ''
-    }
+class ZOVUI(FloatLayout):
+    def JavaStarting(self):
+        minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
+        version = '1.16.5'
+        username = 'kilka'
 
-    subprocess.call(minecraft_launcher_lib.command.get_minecraft_command(version=version, minecraft_directory=minecraft_directory, options=options))
+        minecraft_launcher_lib.install.install_minecraft_version(versionid=version, minecraft_directory=minecraft_directory)
+        options = {
+            'username': username,
+            'uuid': str(uuid1()),
+            'token': ''
+        }
+
+        subprocess.call(minecraft_launcher_lib.command.get_minecraft_command(version=version, minecraft_directory=minecraft_directory, options=options))
+        print('feafea')
 
 
+class MyApp(App):
+    def build(self):
+        Builder.load_file('ZOVUI.kv')
+        return ZOVUI()
 
-
-
-
-
-if __name__ == "__main__":
- run()
+if __name__ == '__main__':
+    MyApp().run()
