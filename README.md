433b9e53e2369405293217740c7e47abeb32e488
Kilka <alak6543@gmail.com>
2024-07-28 01:32:28 +0300

    Удаление QT

--- README.md
+++
@@ -1,3 +0,0 @@
-**ROADMAP**
-
-![Group 1road (1)](https://github.com/user-attachments/assets/85ebe71b-dd9b-4129-a30e-acbaf855553e)
--- main.py
+++ main.py
@@ -1,37 +1,23 @@
 import sys
 import minecraft_launcher_lib
-from PySide6.QtWidgets import QApplication, QMainWindow
-from ZOVUI import Ui_MainWindow
 import subprocess
-from uuid import uuid1
+
 
 minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
 
+def run():
+    version = input('vers')
+    username = input('name')
 
 
+    minecraft_launcher_lib.install.install_minecraft_version(versionid=version, minecraft_directory=minecraft_directory)
+    options = {
+        'username': username,
+        'uuid': str(uuid1()),
+        'token': ''
+    }
 
-class ExpenseTracker(QMainWindow):
-    def __init__(self):
-        super(ExpenseTracker, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-
-        self.ui.PlayButtonJAVA.clicked.connect(self.run)
-        self.setStyleSheet(f"QMainWindow {{ border-image: url('Image (1).png') 0 0 0 0 stretch stretch; }}")
-
-    def run(self):
-        version = input('vers')
-        username = input('name')
-
-
-        minecraft_launcher_lib.install.install_minecraft_version(versionid=version, minecraft_directory=minecraft_directory)
-        options = {
-            'username': username,
-            'uuid': str(uuid1()),
-            'token': ''
-        }
-
-        subprocess.call(minecraft_launcher_lib.command.get_minecraft_command(version=version, minecraft_directory=minecraft_directory, options=options))
+    subprocess.call(minecraft_launcher_lib.command.get_minecraft_command(version=version, minecraft_directory=minecraft_directory, options=options))
 
 
 
@@ -40,8 +26,4 @@ class ExpenseTracker(QMainWindow):
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = ExpenseTracker()
-    window.show()
-
-    sys.exit(app.exec())
+ run()
