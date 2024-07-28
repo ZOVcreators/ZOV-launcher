dbf694a9af9273d81519aeca5e6809ad32f1f2ea
Kilka <alak6543@gmail.com>
2024-07-28 04:19:58 +0300

    Продолжение создания Интерфейса

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 
-class MyWidgetxxx(BoxLayout):
+class MyWidget(BoxLayout):
     pass
 
 class MyApp(App):
     def build(self):
-        return MyWidgetxxx()
+        return MyWidget()
     Builder.load_file("test.kv")
 
 if __name__ == "__main__":
